646f9cb22b213267860afb265a7af17edc1c76d6
Sean Cheetham <cheethas@tcd.ie>
2021-01-17 20:58:23 +0000

    Add more tests

--- test/MyToken.test.js
+++ test/MyToken.test.js
@@ -20,18 +20,40 @@ const expect = chai.expect;
 
 contract("Token Contract", (accounts) => {
 
+    // delegate accounts
+    const [deployerAccount, recipientAccount, anotherAccount] = accounts;
+
     it("All tokens should be in my account", async() => {
         // get the token instance from truffle
         const tokenInstance = await MyToken.deployed();
         // this is a solidity getter and so can easily be accessed in this fashion
         let totalSupply = await tokenInstance.totalSupply();
         // expect the balance of the account that created to contract to be the total contract amount
-        expect(tokenInstance.balanceOf(accounts[0])).to.eventually.be.a.bignumber.equal(totalSupply);
+        expect(tokenInstance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
     });
 
-    // it("Should create a token with a set initial mint", async() => {
+    it("Is possible to send tokens between accounts", async() => {
+        const sendTokens = 1;
+        const tokenInstance = await MyToken.deployed();
+        let totalSupply = await tokenInstance.totalSupply();
+        expect(tokenInstance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
+        
+        // the fufilled keyword for chai basically just wants the promise to finish, and will not care what happens next 
+        expect(tokenInstance.transfer(recipientAccount, sendTokens)).to.eventually.be.fulfilled;
+        expect(tokenInstance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply.sub(new BN(sendTokens)));
+        expect(tokenInstance.balanceOf(recipientAccount)).to.eventually.be.a.bignumber.equal(new BN(sendTokens));
+    });
 
-    //     const tokenInstance = Token.deployed();
+    it("is not possible to send more tokens than are available in total", async() => {
+        const tokenInstance = await MyToken.deployed;
+        let balanceOfDeployer = await tokenInstance.balanceOf(deployerAccount);
+
+        // we will expect this to fail, as there are too many tokens being sent, so we say that it will eventually be rejected
+        expect(tokenInstance.transfer(recipientAccount, new BN(balanceOfDeployer+1))).to.eventually.be.rejected;
+
+        // we expect any transaction that takes place to be rolled back
+        expect(tokenInstance.balanceOf(deployerAccount)).to.eventually.be.equal(balanceOfDeployer);
+    });
 
 
     // })
